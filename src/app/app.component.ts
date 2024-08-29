4aed4348f7862ea3721c177cfdc5628c82be70a6
Sergio Enjuanes <srgenjuanes@gmail.com>
2024-08-29 11:12:35 +0200

    Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChgvwHapfsFaYEGvliDJO8ZLIaY1dU2Nk",
+  authDomain: "strolikiaus.firebaseapp.com",
+  projectId: "strolikiaus",
+  storageBucket: "strolikiaus.appspot.com",
+  messagingSenderId: "657912543133",
+  appId: "1:657912543133:web:5322c51fe2cdaec1b62174",
+  measurementId: "G-X5S8VS0T8P"
+};
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +24,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'strolikiaus';
+
+  constructor() {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
